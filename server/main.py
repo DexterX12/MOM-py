31091caf7ccf71b1518e41386ff185430e42467c
DexterX12 <65998342+DexterX12@users.noreply.github.com>
2025-04-21 09:07:11 -0500

    Removing auth feature inside MOM servers - Auth standalone-server now exists

--- server/main.py
+++ server/main.py
@@ -17,7 +17,6 @@ topics_exchange = {} #dictionary that stores messages in topics with an associat
 user_queues = defaultdict(list)
 
 #app.config.from_prefixed_env() # ENVIROMENT VARIABLE FLASK_SECRET_KEY
-app.secret_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lock = threading.Lock()
 zkcl = None
 
@@ -30,54 +29,21 @@ def welcoming():
     zkcl.track_partition(key)
     return "<h1>MOM test</h1>"
 
-#-------------------------Log in--------------------------
-
-@app.route("/auth", methods=["POST"])
-def log_in():
-    data = request.json
-    print(data)
-    username = data.get("user")
-    pw = data.get("pass")
-    user = auth(username, pw)
-
-    if not user:
-        return jsonify({"error": "Invalid credentials!"}), 401
-    
-    # expects and ID from user as bearer identifier
-    auth_token = jwt.encode({"id": user["id"]}, app.secret_key, algorithm="HS256")
-    return jsonify({"token": auth_token}), 200
-
 #-------------------------Post--------------------------
 
 @app.route("/", methods=["POST"])
 def post():
+    history = []
     data = request.json
     msg_type = data.get("type")
     operation = data.get("operation")
-    auth = request.headers.get("Authorization")
-    user = None
+    user = data.get("username")
+    history.append(user)
 
-    if not auth:
-        response = Response()
-        response.status_code = 401
-        response.headers["WWW-Authenticate"] = "Bearer"
 
-        return response
-    elif len(auth.split(" ")) < 2:
-        return jsonify({"error": "No token provided"}), 401
-    else:
-        try:
-            token = auth.split(" ")[1]
-
-            # Not failing means they are who they claim to be
-            user = jwt.decode(token, app.secret_key, algorithms="HS256")
-            history = []
-            history.append(user)
-            
-        except jwt.exceptions.InvalidTokenError:
-            return jsonify({"error": "Invalid token provided"}), 401
+    if not user:
+        return jsonify({"error": "Could not identify the user making the request"})
         
-
     
     if operation not in ["pull", "push", "subscribe"]:
         return jsonify({"error": "Invalid operation :("}), 400
